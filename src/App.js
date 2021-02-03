9b45b4864ef15ba7c708c567836d607ae90798bf
pagetribe <pagetribemail@gmail.com>
2021-02-03 13:34:00 +1100

    react hook form, reset & validation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { useForm } from 'react-hook-form'
 import { Button, Container, TextField, Grid } from '@material-ui/core'
 import AddIcon from '@material-ui/icons/Add'
 import './App.css'
 
 function App() {
+  const emailInputRef = useRef()
+  const { register, handleSubmit, errors, formState, reset, watch } = useForm()
+  const watchAllFields = watch()
+
+  const onSubmit = (data) => {
+    console.log(data)
+    console.log(formState)
+  }
+
+  const handleReset = () => {
+    reset()
+    emailInputRef.current.focus()
+  }
+
   return (
 
     <Container maxWidth="sm" align="center">
-      <form noValidate autoComplete="off">
+      <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
         <Grid container spacing={3}>
           <Grid item xs={6}>
-            <TextField id="standard-basic" label="Enter an email ID" />
+            <TextField id="standard-basic" label="Enter an email ID" name="email" type="email"
+              inputRef={(e) => {
+                emailInputRef.current = e
+                register(e, {
+                  required: true,
+                  pattern: {
+                    value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                    message: "Email is not valid."
+                  }
+                })
+              }}
+              // autoFocus
+              error={!!errors.email}
+              helperText={errors.email && errors.email.message}
+            />
+
+            {errors.email && <p>{errors.email.message}</p>}
           </Grid>
           <Grid item xs={6}>
             <Button
@@ -24,8 +55,8 @@ function App() {
             </Button>
           </Grid>
           <Grid item xs={12}>
-            <Button color="primary" disabled>Reset</Button>
-            <Button color="primary">Email</Button>
+            <Button color="primary" disabled={!formState.isDirty || watchAllFields.email === ""} onClick={handleReset}>Reset</Button>
+            <Button color="primary" type="submit">Email</Button>
           </Grid>
         </Grid>
       </form>
